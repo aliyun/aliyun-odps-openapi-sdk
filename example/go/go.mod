fd5368d4269476b5bad4dfe42e5929e2f54c301b
鼎昕 <zhangdingxin.zdx@alibaba-inc.com>
2025-01-13 18:46:17 +0800

    example(catalog-api): add golang example

--- example/go/go.mod
+++ example/go/go.mod
@@ -2,4 +2,20 @@ module _go
 
 go 1.23.2
 
-require github.com/aliyun/aliyun-odps-openapi-sdk/catalog-api/go v0.0.0-20250113090952-9faa6dbf1db7
+require (
+	github.com/aliyun/aliyun-odps-openapi-sdk/catalog-api/go v0.0.0-20250113090952-9faa6dbf1db7
+	github.com/aliyun/aliyun-odps-openapi-sdk/common/go v0.0.0-20250113095840-a4728163e1f6
+)
+
+require (
+	github.com/alibabacloud-go/debug v1.0.1 // indirect
+	github.com/alibabacloud-go/tea v1.2.2 // indirect
+	github.com/alibabacloud-go/tea-utils/v2 v2.0.6 // indirect
+	github.com/aliyun/aliyun-odps-go-sdk/tea v0.0.0-20250113093743-81345ae38a07 // indirect
+	github.com/aliyun/credentials-go v1.3.10 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
--- example/go/table_test.go
+++ example/go/table_test.go
@@ -8,26 +8,60 @@ import (
 	openapi "github.com/aliyun/aliyun-odps-openapi-sdk/common/go/client"
 )
 
-func Test_GetTable(t *testing.T) {
+const endpoint = ""
+const accessId = ""
+const accessKey = ""
+
+func Test_CreateTable(t *testing.T) {
 	var config openapi.Config
-	config.SetAccessKeyId("")
-	config.SetAccessKeySecret("")
-	config.SetEndpoint("")
+	config.SetAccessKeyId(accessId)
+	config.SetAccessKeySecret(accessKey)
+	config.SetEndpoint(endpoint)
 
 	newClient, err := client.NewClient(&config)
 	if err != nil {
 		log.Fatal("get client error: ", err)
 	}
 
-	var table *client.Table
-	table.SetProjectId("")
-	table.SetSchemaName("")
-	table.SetTableName("")
+	var table client.Table
+	table.SetType("TABLE")
+	table.SetProjectId("project")
+	table.SetSchemaName("default")
+	table.SetTableName("test")
 
-	table, err = newClient.GetTable(table)
+	var tableSchema client.TableFieldSchema
+	var column0 client.TableFieldSchema
+	column0.SetTypeCategory("STRING")
+	column0.SetFieldName("c0")
+	tableSchema.SetFields([]*client.TableFieldSchema{&column0})
+	table.SetTableSchema(&tableSchema)
+
+	tableRef, err := newClient.CreateTable(&table)
+	if err != nil {
+		log.Fatal("create table error: ", err)
+	}
+	println(*tableRef.Name)
+}
+
+func Test_GetTable(t *testing.T) {
+	var config openapi.Config
+	config.SetAccessKeyId(accessId)
+	config.SetAccessKeySecret(accessKey)
+	config.SetEndpoint(endpoint)
+
+	newClient, err := client.NewClient(&config)
+	if err != nil {
+		log.Fatal("get client error: ", err)
+	}
+
+	var table client.Table
+	table.SetProjectId("project")
+	table.SetSchemaName("default")
+	table.SetTableName("test")
+
+	tableRef, err := newClient.GetTable(&table)
 	if err != nil {
 		log.Fatal("get table error: ", err)
 	}
-
-	println(table.Name)
+	println(*tableRef.Name)
 }
